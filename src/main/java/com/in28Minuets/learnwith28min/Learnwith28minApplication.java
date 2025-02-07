277ad20b24e77f94fe9297ac4a05c52a396cf2fd
Monsingozzy <mohansinghyoyo88@gmail.com>
2025-02-07 22:12:58 +0530

    creating spring

---
+++ src/main/java/com/in28Minuets/learnwith28min/Learnwith28minApplication.java
@@ -0,0 +1,13 @@
+package com.in28Minuets.learnwith28min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learnwith28minApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learnwith28minApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28Minuets/learnwith28min/Learnwith28minApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28Minuets.learnwith28min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learnwith28minApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
